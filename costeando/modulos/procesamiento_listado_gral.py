804ef2e2976066869fb4b03edcb71254a2d85c8d
tomassalvatierra <salvatierratomaslahuel@gmail.com>
2025-09-02 16:31:51 -0300

    Se modifico el procesamiento del listado_general para que elimine los TIPO DESCUENTO que no tengan descuento. Se agregaron columnas sin datos necesarias,y se agrego una comprobacion de la existencia de la columna Costo Estandar o COSTO LISTA ACC.

--- costeando/modulos/procesamiento_listado_gral.py
+++ costeando/modulos/procesamiento_listado_gral.py
@@ -33,11 +33,8 @@ def procesar_listado_gral_puro(
     carpeta_guardado: str
 ) -> Dict[str, str]:
     
-    """
-    Procesa el módulo Listado general y guarda los archivos generados en la carpeta indicada.
-    Devuelve un diccionario con los paths de los archivos generados.
-    """
-    logger.info("Iniciando procesamiento puro de Leader List")
+    
+    logger.info("Iniciando procesamiento puro de Listado General")
     campania = campania.zfill(2)
     
     lista = [
@@ -71,7 +68,6 @@ def procesar_listado_gral_puro(
     
     try:
         
-
         lista_df = [
             (df_produciendo, 'produciendo'),
             (df_costo_primo, 'costo_primo'),
@@ -88,14 +84,20 @@ def procesar_listado_gral_puro(
         
         df_listado_general = df_listado.copy()
         
-      
-        df_listado_general.rename(columns={"Costo Estandard":"COSTO LISTA " + anio[-1] +campania}, inplace= True)
+        #Si la columna "Costo Estandard" existe, renombrarla, sino comprobar que COSTO LISTA " + anio[-1] +campania  exista
+        if "Costo Estandard" in df_listado_general.columns:
+            logger.debug("Renombrando columna 'Costo Estandard' a 'COSTO LISTA ...'")
+            df_listado_general.rename(columns={"Costo Estandard":"COSTO LISTA " + anio[-1] +campania}, inplace= True)
+        else:
+            if "COSTO LISTA " + anio[-1] +campania not in df_listado_general.columns:
+                error_msg = f"Ninguna de las columnas 'Costo Estandard' o 'COSTO LISTA {anio[-1]}{campania}' existe en el listado."
+                logger.error(error_msg)
+                raise ValueError(error_msg)
         
         df_mdo_806 = df_mdo.loc[df_mdo["Componente"].isin(["MOD0806"])]
         df_mdo_807 = df_mdo.loc[df_mdo["Componente"].isin(["MOD0807"])]
         df_mdo_808 = df_mdo.loc[df_mdo["Componente"].isin(["MOD0808"])]
-        
-
+    
         #Merge con Maestro Costo Primo
         logger.debug("Realizando merges de MANO DE OBRA TOTAL")
         df_listado_general = pd.merge(df_listado_general,df_costo_primo[["Codigo","Costo Estand"]], how="left", on="Codigo")
@@ -116,7 +118,6 @@ def procesar_listado_gral_puro(
         #Merge con CALCULO PRODUCIENDO
         logger.debug("Realizando merge costo de producción y CARGA FABRIL")
         df_listado_general = pd.merge(df_listado_general,df_produciendo[["Codigo", "Costo Producción"]], how="left", on="Codigo")
-        #df_listado_general = pd.merge(df_listado_general,df_produciendo[["Codigo", "Lleva CF?"]], how="left", on="Codigo")
 
         # Selecciona las columnas relevantes
         cols = ["Codigo", "Costo sin Descuento C"+campania, "% de obsolescencia", "ROYALTY", "DESCUENTO ESPECIAL", "APLICA DDE CA:"]
@@ -142,7 +143,10 @@ def procesar_listado_gral_puro(
 
         #Merge con Base de Descuentos
         df_listado_general = pd.merge(df_listado_general,df_base_descuentos[["Codigo", "TIPO-DESCUENTO"]], how="left", on="Codigo")
-
+        print(df_listado_general.info())
+        #Si el DESCUENTO ESPECIAL es cero quitar el TIPO-DESCUENTO
+        df_listado_general.loc[df_listado_general["DESCUENTO ESPECIAL"].fillna(0) == 0, "TIPO-DESCUENTO"] = None
+        
         #Merge con Base de Descuentos
         logger.debug("Realizando merges con el leader list")
         df_listado_general = pd.merge(df_listado_general,df_leader_list[["Codigo", "TIPO_OF"]], how="left", on="Codigo")
@@ -177,10 +181,11 @@ def procesar_listado_gral_puro(
         
         df_listado_general["MANO DE OBRA TOTAL"] = df_listado_general["MANO DE OBRA TOTAL"].fillna(0)
         df_listado_general["CARGA FABRIL"] = df_listado_general["CARGA FABRIL"].fillna(0)
-
+        df_listado_general["COD MADRE"] = ""
+        df_listado_general["COD COMB"] = ""
         # Reindexar columnas según el orden solicitado
         columnas_ordenadas = [
-            "Periodo", "Codigo", "COD MADRE", "COD COMB", "Descripcion", "COSTO PRIMO (MATERIALES)", "MANO DE OBRA TOTAL", "Costo Producción", "CARGA FABRIL", "Costo sin Descuento C" + campania, "% Sumatoria de Descuentos", "COSTO LISTA " + anio[-1] +campania, "TIPO COSTO","ADI N°", "Ult. Compra", "TIPO ULT COMPRA", "MOD 0806 SEGUNDOS MO ELAB. X KILO", "MOD 0807 SEGUNDOS MO ENV. X UNIDAD", "MOD 0808 SEGUNDOS MO ACOND.X UNIDAD", "% de obsolescencia", "ROYALTY", "DESCUENTO ESPECIAL", "APLICA DDE CA:", "TIPO-DESCUENTO", "DESCUENTO APLICADO $", "Stock Actual", "VALOR STOCK CON DESCUENTO", "TIPO_OF", "LEYEOFE", "Estado", "Cod Actualiz", "VARIABLE", "LLEVA CF", "Tipo", "Desc.Tipo", "Grupo", "Desc. Grupo", "Sub Grupo", "Desc.Sub Grupo", "Entra MRP", "Atiende Necsdd", "Prov", "Ult P/C", "Razon Social", "Fecha Alta"
+            "Periodo", "Codigo", "COD MADRE", "COD COMB", "Descripcion", "COSTO PRIMO (MATERIALES)", "MANO DE OBRA TOTAL", "Costo Producción", "CARGA FABRIL", "Costo sin Descuento C" + campania, "% Sumatoria de Descuentos", "COSTO LISTA " + anio[-1] +campania, "TIPO DE COSTO","ADI N°", "Ult. Compra", "TIPO ULT COMPRA", "MOD 0806 SEGUNDOS MO ELAB. X KILO", "MOD 0807 SEGUNDOS MO ENV. X UNIDAD", "MOD 0808 SEGUNDOS MO ACOND.X UNIDAD", "% de obsolescencia", "ROYALTY", "DESCUENTO ESPECIAL", "APLICA DDE CA:", "TIPO-DESCUENTO", "DESCUENTO APLICADO $", "Stock Actual", "VALOR STOCK CON DESCUENTO", "TIPO_OF", "LEYEOFE", "Estado", "Cod Actualiz", "VARIABLE", "LLEVA CF", "Tipo", "Desc.Tipo", "Grupo", "Desc. Grupo", "Sub Grupo", "Desc.Sub Grupo", "Entra MRP", "Atiende Necsdd", "Prov", "Ult P/C", "Razon Social", "Fecha Alta"
         ]
         # Reindex solo si existen las columnas, las que falten se ignoran
         columnas_existentes = [col for col in columnas_ordenadas if col in df_listado_general.columns]
